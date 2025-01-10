128bf0ecc59e5c63a7dfb286fb39f92bb37f7834
zmatthews33 <zmatthews@zanderins.com>
2025-01-10 15:35:44 -0600

    added NEW/edited exercisePaths

--- src/data/exercisePaths.ts
+++ src/data/exercisePaths.ts
@@ -14,68 +14,69 @@ export const exercisePaths: ExercisePath[] = [
     AnkleTestFail: true,
     CalfTestFail: true,
     WorkoutPerWeekNumber: 2,
-    Workout1: "Calf Strength, Knee Strength, Gluteus Medius Strength, Foot Strength",
-    Workout2: "Ankle Mobility, Hamstring Strength, Gluteus Maximus Strength, Balance"
+    Workout1: "Knee Strength, Gluteus Medius Strength, Ankle Mobility, Balance, Calf Strength, Hamstring Strength",
+    Workout2: "Knee Strength, Gluteus Maximus Strength, Ankle Mobility, Balance, Calf Strength, Hamstring Strength"
   },
   {
     Path: 2,
     AnkleTestFail: true,
     CalfTestFail: false,
     WorkoutPerWeekNumber: 2,
-    Workout1: "Knee Strength, Gluteus Medius Strength, Hamstring Strength, Foot Strength",
-    Workout2: "Gluteus Maximus Strength, Balance, Ankle Mobility, Foot Strength"
+    Workout1:
+      "Knee Strength, Gluteus Medius Strength, Gluteus Maximus Strength, Ankle Mobility, Balance, Hamstring Strength",
+    Workout2: "Knee Strength, Gluteus Medius Strength, Gluteus Maximus Strength, Ankle Mobility, Balance, Front Plank"
   },
   {
     Path: 3,
     AnkleTestFail: false,
     CalfTestFail: true,
     WorkoutPerWeekNumber: 2,
-    Workout1: "Calf Strength, Knee Strength, Gluteus Medius Strength, Hamstring Strength",
-    Workout2: "Gluteus Maximus Strength, Balance, Knee Strength Isometrics, Foot Strength"
+    Workout1: "Knee Strength, Gluteus Medius Strength, Gluteus Maximus Strength, Balance, Hamstring, Calf Strength",
+    Workout2: "Knee Strength, Gluteus Medius Strength, Gluteus Maximus Strength, Balance, Calf Strength, Front Plank"
   },
   {
     Path: 4,
     AnkleTestFail: false,
     CalfTestFail: false,
     WorkoutPerWeekNumber: 2,
-    Workout1: "Knee Strength, Gluteus Medius Strength, Hamstring Strength, Foot Strength",
-    Workout2: "Gluteus Maximus Strength, Balance, Knee Strength Isometrics, Foot Strength"
+    Workout1: "Knee Strength, Gluteus Medius Strength, Gluteus Maximus Strength, Balance, Hamstring",
+    Workout2: "Knee Strength, Gluteus Medius Strength, Gluteus Maximus Strength, Balance, Front Plank"
   },
   {
     Path: 5,
     AnkleTestFail: true,
     CalfTestFail: true,
     WorkoutPerWeekNumber: 3,
-    Workout1: "Calf Strength, Knee Strength, Foot Strength",
-    Workout2: "Hamstring Strength, Gluteus Maximus Strength, Ankle Mobility",
-    Workout3: "Front Planks, Balance, Gluteus Medius Strength"
+    Workout1: "Calf Strength, Front Plank, Balance, Knee Strength, Gluteus Medius Strength",
+    Workout2: "Calf Strength, Ankle Mobility, Gluteus Medius, Gluteus Maximus Strength",
+    Workout3: "Ankle Mobility, Hamstring Strength, Balance, Knee Strength, Gluteus Maximus Strength"
   },
   {
     Path: 6,
     AnkleTestFail: true,
     CalfTestFail: false,
     WorkoutPerWeekNumber: 3,
-    Workout1: "Knee Strength, Hamstring Strength, Foot Strength",
-    Workout2: "Hamstring Strength, Gluteus Maximus Strength, Ankle Mobility",
-    Workout3: "Front Planks, Balance, Gluteus Medius Strength"
+    Workout1: "Front Plank, Balance, Knee Strength, Gluteus Medius Strength",
+    Workout2: "Ankle Mobility, Gluteus Medius Strength, Gluteus Maximus Strength, Hamstring Strength",
+    Workout3: "Ankle Mobility, Balance, Knee Strength, Gluteus Maximus Strength"
   },
   {
     Path: 7,
     AnkleTestFail: false,
     CalfTestFail: true,
     WorkoutPerWeekNumber: 3,
-    Workout1: "Calf Strength, Knee Strength, Hamstring Strength",
-    Workout2: "Gluteus Maximus Strength, Knee Strength Isometrics, Foot Strength",
-    Workout3: "Front Planks, Balance, Gluteus Medius Strength"
+    Workout1: "Calf Strength, Front Plank, Balance, Knee Strength, Gluteus Medius Strength",
+    Workout2: "Calf Strength, Balance, Gluteus Medius, Gluteus Maximus Strength",
+    Workout3: "Ankle Mobility, Hamstring Strength, Knee Strength, Gluteus Maximus Strength"
   },
   {
     Path: 8,
     AnkleTestFail: false,
     CalfTestFail: false,
     WorkoutPerWeekNumber: 3,
-    Workout1: "Knee Strength, Hamstring Strength, Foot Strength",
-    Workout2: "Gluteus Maximus Strength, Knee Strength Isometrics, Foot Strength",
-    Workout3: "Front Planks, Balance, Gluteus Medius Strength"
+    Workout1: "Front Plank, Balance, Knee Strength, Gluteus Medius Strength",
+    Workout2: "Balance, Gluteus Medius Strength, Gluteus Maximus Strength, Knee Strength",
+    Workout3: "Hamstring Strength, Knee Strength, Gluteus Maximus Strength, Balance"
   }
 ]
 
